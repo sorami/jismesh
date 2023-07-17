ba82d9470870dc2685611954b5da6c4c6f47dfa3
Sorami Hisamoto <hi@sorami.dev>
2023-07-17 22:29:07 +0900

    🗾

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import '@unocss/reset/tailwind-compat.css';
+	import 'virtual:uno.css';
+	import 'maplibre-gl/dist/maplibre-gl.css';
+	import maplibre from 'maplibre-gl';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const map = new maplibre.Map({
+			container: 'map',
+			style: 'https://demotiles.maplibre.org/style.json',
+			center: [139.741357, 35.658099], // 日本経緯度原点
+			zoom: 5
+		});
+
+		map.addControl(new maplibre.NavigationControl());
+		map.addControl(new maplibre.GeolocateControl({}));
+		map.addControl(new maplibre.FullscreenControl());
+	});
+</script>
+
+<div id="map" class="w-screen h-screen" />
